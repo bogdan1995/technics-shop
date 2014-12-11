aa837001fd81af97ba56734ecbddd58a0511d29a
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2014-12-12 01:38:18 +0300

    gulp config ready, livereload doesn't working :(

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,9 @@
 var gulp = require('gulp'), 
 	autoprefixer = require('gulp-autoprefixer'), 
 	clean = require('gulp-clean'),
-	connect = require('gulp-connect'), 
-	csscomb = require('gulp-csscomb'), 
 	gulpif = require('gulp-if'), 
 	jade = require('gulp-jade'), 
 	jshint = require('gulp-jshint'), 
-	jsmin = require('gulp-jsmin'),
 	minifyCss = require('gulp-minify-css'),
 	notify = require('gulp-notify'), 
 	sass = require('gulp-sass'), 
@@ -16,7 +13,8 @@ var gulp = require('gulp'),
 	uncss = require('gulp-uncss'), 
 	useref = require('gulp-useref'),
 	opn = require('opn'), 
-	wiredep = require('wiredep'); 
+	wiredep = require('wiredep'),
+  connect = require('gulp-connect');
 
 gulp.task('connect', function() {
   connect.server({
@@ -27,68 +25,53 @@ gulp.task('connect', function() {
 });
 
 gulp.task('wiredep', function () {    
-  gulp.src('proj/*.html')
+  gulp.src('./proj/*.html')
     .pipe(wiredep({
-        directory: 'proj/bower_components'
+        directory: './proj/bower_components'
     }))
-    .pipe(gulp.dest('proj'));
+    .pipe(gulp.dest('./proj'));
 });
 
-gulp.task('sass', function () {
-    gulp.src('proj/scss/*.scss')
+gulp.task('css', function () {
+    gulp.src('./proj/scss/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('css'))
-        .pipe(notify("SCSS done!"));
-});
-
-gulp.task('autopref', function () {
-    return gulp.src('proj/css/style.css')
         .pipe(autoprefixer({
             browsers: ['last 15 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('proj/css'))
-        .pipe(notify("autopref done!"));
+        .pipe(gulp.dest('./proj/css'))
+        .pipe(notify("SCSS done!"));
 });
 
-gulp.task('comb', function () {
-  return gulp.src('proj/css/style.css')
-    .pipe(csscomb())
-    .pipe(gulp.dest('proj/css'))
-    .pipe(notify("csscomb done!"));
-});
-
-gulp.task('html', function() {
-  gulp.src('proj/jade/_pages/*.jade')
+gulp.task('jade', function() {
+  gulp.src('./proj/jade/_pages/*.jade')
     .pipe(jade({
-      pretty: true
+    	pretty: true
     }))
-    .pipe(gulp.dest('proj/'))
-    .pipe(notify("Jade done!"))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('./proj/'))
+    .pipe(connect.reload())
+    .pipe(notify("Jade done!"));
 });
 
-gulp.task('uncss', function() {
-    return gulp.src('proj/css/style.css')
-        .pipe(uncss({
-            html: glob.sync('proj/*.html')
-        }))
-        .pipe(notify("uncss done!"))
-        .pipe(gulp.dest('proj/css/style.css'))
-        .pipe(connect.reload());
-});
+// gulp.task('uncss', function() {
+//     return gulp.src('./proj/css/style.css')
+//         .pipe(uncss({
+//             html: glob.sync('./proj/*.html')
+//         }))
+//         .pipe(notify("uncss done!"))
+//         .pipe(gulp.dest('./proj/css'));
+// });
 
 gulp.task('jshint', function() {
-  return gulp.src('proj/js/*.js')
+  return gulp.src('./proj/js/*.js')
     .pipe(jshint())
-    .pipe(notify("JS done!"))
-    .pipe(connect.reload());
+    .pipe(notify("JS done!"));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['proj/jade/_pages/*.jade'], ['html']);
-  gulp.watch(['proj/scss/*.scss'], ['sass', 'autopref', 'comb', 'uncss']);
-  gulp.watch(['proj/js/*.js'], ['jshint']);
+  gulp.watch(['./proj/jade/_pages/*.jade'], ['jade']);
+  gulp.watch(['./proj/scss/**/*.scss'], ['css']);
+  gulp.watch(['./proj/js/*.js'], ['jshint']);
   gulp.watch('bower.json', ['wiredep']);
 });
 
@@ -108,4 +91,4 @@ gulp.task('clean', function () {
 });
 
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect','watch']);
