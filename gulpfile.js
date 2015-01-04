50ceaab02083a1df07f25aef625fbf3b15e65998
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2015-01-05 00:23:06 +0300

    add support SASS Compass and other SASS frameworks

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,69 @@
 "use strict";
 
-var gulp = require('gulp'), 
-	autoprefixer = require('gulp-autoprefixer'), 
-	clean = require('gulp-clean'),
-	gulpif = require('gulp-if'), 
-	jade = require('gulp-jade'), 
-	jshint = require('gulp-jshint'), 
-	minifyCss = require('gulp-minify-css'),
-	notify = require('gulp-notify'), 
-	sass = require('gulp-sass'), 
-	uglify = require('gulp-uglify'),
-	uncss = require('gulp-uncss'), 
-	useref = require('gulp-useref'),
-	opn = require('opn'), 
-	wiredep = require('wiredep').stream,
-  connect = require('gulp-connect');
+var
+    gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    clean = require('gulp-clean'),
+    gulpif = require('gulp-if'),
+    jade = require('gulp-jade'),
+    jshint = require('gulp-jshint'),
+    minifyCss = require('gulp-minify-css'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    uncss = require('gulp-uncss'),
+    useref = require('gulp-useref'),
+    opn = require('opn'),
+    wiredep = require('wiredep').stream,
+    connect = require('gulp-connect'),
+    compass = require('gulp-compass');
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'proj',
-    livereload: true,
-    port: 8000
-  });
-  opn('http://localhost:8000/');
+gulp.task('connect', function () {
+    connect.server({
+        root: 'proj',
+        livereload: true,
+        port: 8000
+    });
+    opn('http://localhost:8000/');
 });
 
-
-
-gulp.task('css', function () {
-    gulp.src('./proj/scss/*.scss')
-        .pipe(sass())
+gulp.task('css', function() {
+    gulp.src('proj/scss/**/*.scss')
+        .pipe(compass({
+            css: 'proj/css',
+            sass: 'proj/scss',
+            require: ['compass']
+        }))
         .pipe(autoprefixer({
             browsers: ['last 15 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./proj/css'))
+        .pipe(gulp.dest('proj/css'))
         .pipe(connect.reload())
         .pipe(notify("SCSS done!"));
 });
 
-gulp.task('jade', function() {
-  gulp.src('./proj/jade/_pages/*.jade')
-    .pipe(jade({
-    	pretty: true
-    }))
-    .pipe(gulp.dest('./proj/'))
-    .pipe(connect.reload())
-    .pipe(notify("Jade done!"));
+
+//gulp.task('css', function () {
+//    gulp.src('./proj/scss/*.scss')
+//        .pipe(sass())
+//        .pipe(autoprefixer({
+//            browsers: ['last 15 versions'],
+//            cascade: false
+//        }))
+//        .pipe(gulp.dest('./proj/css'))
+//        .pipe(connect.reload())
+//        .pipe(notify("SCSS done!"));
+//});
+
+gulp.task('jade', function () {
+    gulp.src('./proj/jade/_pages/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./proj/'))
+        .pipe(connect.reload())
+        .pipe(notify("Jade done!"));
 });
 
 // gulp.task('uncss', function() {
@@ -58,11 +75,11 @@ gulp.task('jade', function() {
 //         .pipe(gulp.dest('./proj/css'));
 // });
 
-gulp.task('jshint', function() {
-  return gulp.src('./proj/js/*.js')
-    .pipe(jshint())
-    .pipe(connect.reload())
-    .pipe(notify("JS done!"));
+gulp.task('jshint', function () {
+    return gulp.src('./proj/js/*.js')
+        .pipe(jshint())
+        .pipe(connect.reload())
+        .pipe(notify("JS done!"));
 
 });
 
@@ -78,21 +95,21 @@ gulp.task('wiredep', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./proj/jade/_pages/*.jade'], ['jade']);
-  gulp.watch(['./proj/scss/**/*.scss'], ['css']);
-  gulp.watch(['./proj/js/*.js'], ['jshint']);
-  gulp.watch(['./bower.json'], ['wiredep']);
+    gulp.watch(['./proj/jade/_pages/*.jade'], ['jade']);
+    gulp.watch(['./proj/scss/**/*.scss'], ['css']);
+    gulp.watch(['./proj/js/*.js'], ['jshint']);
+    gulp.watch(['./bower.json'], ['wiredep']);
 });
 
 gulp.task('build', function () {
- 	var assets = useref.assets();
-  return gulp.src('proj/*.html')
-    .pipe(assets)
-    .pipe(gulpif('*.js', uglify()))
-    .pipe(gulpif('*.css', minifyCss()))
-    .pipe(assets.restore())
-    .pipe(useref())
-    .pipe(gulp.dest('dist'));
+    var assets = useref.assets();
+    return gulp.src('proj/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
@@ -101,4 +118,4 @@ gulp.task('clean', function () {
 });
 
 
-gulp.task('default', ['connect','watch']);
+gulp.task('default', ['connect', 'watch']);
