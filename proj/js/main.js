6200b8b7aad48395cc7f076391ec3fa7abfa9ce0
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2015-03-13 01:22:20 +0300

    add news block

--- proj/js/main.js
+++ proj/js/main.js
@@ -1,5 +1,19 @@
 "use strict";
 
-(function () {
-	console.log('Hello, World!');
+var app = (function () {
+	return {
+		init: function () {
+			app.setUpListeners();
+		},
+		setUpListeners: function () {
+			$('.slider__list').bxSlider({
+				useCSS: false,
+				controls: false,
+				pager: true
+			});
+			$('.pager-link').text('');
+		}
+	}
 })();
+
+app.init();
